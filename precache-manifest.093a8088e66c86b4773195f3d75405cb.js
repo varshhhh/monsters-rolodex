e5ed17d7fda4d3e7082ba7c35d557c4d9b2ba501
varshhhh <varshney.harshit@gmail.com>
2019-08-25 11:18:32 -0700

    Updates

---
+++ precache-manifest.093a8088e66c86b4773195f3d75405cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13f3dad64cbc181ee41b8cf8a22d3e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f65b308fa04a63ac971c",
+    "url": "/monsters-rolodex/static/css/main.d587464e.chunk.css"
+  },
+  {
+    "revision": "a9690e2a70e33c66e568",
+    "url": "/monsters-rolodex/static/js/2.3df813ea.chunk.js"
+  },
+  {
+    "revision": "f65b308fa04a63ac971c",
+    "url": "/monsters-rolodex/static/js/main.756a7ae8.chunk.js"
+  },
+  {
+    "revision": "e84c251911ba05ca5db3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c77045a6.js"
+  }
+]);
